5041931582b3e373c34817d3bc2b3ec80a0d6924
Denys Borysiuk <denisborisiuk@gmail.com>
2023-12-07 14:23:09 +0200

    fix counter width bug

--- src/components/ui/Counter/Counter.tsx
+++ src/components/ui/Counter/Counter.tsx
@@ -14,17 +14,14 @@ const Counter = () => {
     return () => clearInterval(timer);
   }, [count]);
 
-  //   console.log(count);
-
-  //   const string = count.toString();
-  //   const parts = string.split('.');
   const formattedNumber = formatNumber(count);
-  //   console.log(formattedNumber);
 
   return (
     <div className="flex items-center justify-center gap-[8px] md:gap-[24px]">
-      <p className="font-oswald text-[48px] font-bold leading-none text-accent md:text-[100px] xl:text-[164px]">
-        {/* 1.134.147.814 */}
+      <p
+        className="w-[256px] font-oswald text-[48px] font-bold leading-none text-accent md:w-[533px]
+        md:text-[100px] xl:w-[874px] xl:text-[164px]"
+      >
         {formattedNumber}
       </p>
       <span className="font-oswald text-[24px] leading-none md:text-[28px] xl:text-[48px] mdOnly:leading-[1.7]">
