d1d65038ef856bdb482627710cf2aeba0c89cde0
Denys Borysiuk <denisborisiuk@gmail.com>
2023-12-13 00:14:32 +0200

    fix img in cases

--- src/components/ui/CasesCard/CasesCard.tsx
+++ src/components/ui/CasesCard/CasesCard.tsx
@@ -1,31 +1,26 @@
+'use client';
+
+import Image from 'next/image';
 import Arrow from 'public/icons/arrow-xl.svg';
 import { CasesCardProps } from './type';
 
 const CasesCard = ({ img, title, text, date }: CasesCardProps) => {
   return (
     <div className="h-[318px] md:h-[322px] md:w-[342px] xl:h-[506px] xl:w-[596px] ">
-      <div className="h-[168px]  xl:h-[296px]">
-        <picture>
-          <source
-            srcSet={`
-                /images/cases/${img}_lg@1x.webp 1x,
-                /images/cases/${img}_lg@2x.webp 2x
-  `}
-            type="image/webp"
-          />
-          <img
-            className="h-full w-full object-cover"
-            src={`/images/cases/${img}_lg@1x.webp`}
-            alt={text}
-            loading="lazy"
-          />
-        </picture>
+      <div className="relative h-[168px]  xl:h-[296px]">
+        <Image
+          src={`/images/cases/${img}_lg@2x.webp`}
+          alt={text}
+          fill
+          sizes="(max-width: 767px) 320px, (max-width: 1279px) 342px, 596px"
+          quality={90}
+        />
       </div>
       <div className="bg-form_bg px-[12px] pb-[12px] pt-[24px] xl:px-[48px] xl:py-[36px]">
         <div className="mb-[24px] flex justify-between md:mb-[22px] xl:mb-[36px]">
           <p
             className="h-[66px] w-[175px] text-[18px] tracking-[-0.72px] 
-          md:h-[72px] md:w-[230px] md:text-[20px] xl:w-[357px] xl:text-[24px]"
+            md:h-[72px] md:w-[230px] md:text-[20px] xl:w-[357px] xl:text-[24px]"
           >
             {title}
           </p>
