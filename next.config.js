5832ed9c0b63f94b858eec6624d9e5707e709cec
Denys Borysiuk <denisborisiuk@gmail.com>
2023-12-15 22:46:00 +0200

    fix og

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
     deviceSizes: [
       320, 342, 360, 480, 596, 768, 1024, 1280, 1366, 1440, 1920, 2048,
     ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+      },
+    ],
   },
 };
 
