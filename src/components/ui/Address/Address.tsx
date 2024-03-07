03744d03dff1a0b0db151e7fd196eb8fa783b494
Denys Borysiuk <denisborisiuk@gmail.com>
2024-03-07 14:45:11 +0200

    fix footer

--- src/components/ui/Address/Address.tsx
+++ src/components/ui/Address/Address.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link';
-import { AddressProps } from './type';
 
-const Address = ({ className }: AddressProps) => {
+const Address = () => {
   return (
     <div className="flex items-center smOnly:flex-col smOnly:gap-[8px]">
       <address className="not-italic md:mr-auto">
         79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
       </address>
+
       <Link href="mailto:office@ecosolution.com">office@ecosolution.com</Link>
-      <p className={`${className} md:ml-[50px] md:flex xl:ml-[167px]`}>
+
+      <p className="md:ml-[50px] md:flex xl:ml-[167px]">
         ecosolution &copy; 2023
       </p>
     </div>
--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -5,13 +5,16 @@ import ButtonUp from '@/components/ui/ButtonUp/ButtonUp';
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-accent pt-[24px] md:pt-[40px]">
+    <footer className="border-t border-accent py-[24px] md:py-[40px]">
       <div className="mb-[24px] flex flex-wrap md:items-center smOnly:justify-between smOnly:gap-y-[24px]">
         <Logo className="w-[269px] md:order-1 md:mr-[97px] xl:mr-[512px]" />
+
         <ButtonUp />
+
         <SocialIcons className="flex justify-center gap-[8px] md:order-2  md:mr-auto smOnly:w-full" />
       </div>
-      <Address className="mb-[36px]" />
+
+      <Address />
     </footer>
   );
 };
